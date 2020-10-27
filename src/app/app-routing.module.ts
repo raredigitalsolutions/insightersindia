8848fc282f9355dbc6d01f77c650fb04f0c3d863
Gokul Ram T J <gokulram.t.j@gmail.com>
2020-10-27 13:55:50 +0530

    [ADD] Parallax

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { MaskComponent } from './pages/home/mask/mask.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }
+  { path: '', component: MaskComponent }
+  // { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }
 ];
 
 @NgModule({
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,10 +5,11 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LandingIllustrationComponent } from './landing-illustration/landing-illustration.component';
 import { BannerComponent } from './banner/banner.component';
+import { MaskComponent } from './mask/mask.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, LandingIllustrationComponent, BannerComponent],
+  declarations: [HomeComponent, LandingIllustrationComponent, BannerComponent, MaskComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
